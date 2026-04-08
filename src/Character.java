7e38695e1654313106afe7865232f72d3308ecba
Petarcho29 <petarkarpachev29@gmail.com>
2026-04-08 19:43:27 +0300

    Initial commit

---
+++ src/Character.java
@@ -0,0 +1,18 @@
+public abstract class Character {
+    protected String type;
+    protected int health;
+    protected int energy;
+    protected final int maxEnergy;
+    protected boolean canUseGadget;
+
+    public Character(String type, int health) {
+        this.type = type;
+        this.energy = 0;
+        this.health = health;
+        this.maxEnergy = 100;
+        this.canUseGadget = true;
+    }
+    protected abstract void basic(Character target);
+    protected abstract boolean ultimate(Character target);
+    protected abstract boolean gadget(Character target);
+}
---
+++ src/Warrior.java
@@ -0,0 +1,42 @@
+public class Warrior extends Character{
+    private boolean gadget;
+    public Warrior(String type, int health) {
+        super("Warrior", 1200);
+        gadget = false;
+    }
+
+    @Override
+    protected void basic(Character target) {
+        System.out.println("Warrior dealt 120 damage to " + target.type);
+        target.health -= 120;
+    }
+
+    @Override
+    protected boolean ultimate(Character target) {
+        if (this.energy >= 50) {
+            System.out.println("Warrior dealt 260 damage to " + target.type);
+            target.health -= 260;
+            this.energy -= 50;
+            return false;
+        }
+        else {
+            System.out.println("Not enough energy!");
+            return true;
+        }
+    }
+
+    @Override
+    protected boolean gadget(Character target) {
+        if (this.canUseGadget) {
+            System.out.println("Gadget activated!");
+            this.gadget = true;
+            this.canUseGadget = false;
+            return false;
+        }
+        else {
+            System.out.println("The gadget is already used!");
+            return true;
+        }
+    }
+
+}
